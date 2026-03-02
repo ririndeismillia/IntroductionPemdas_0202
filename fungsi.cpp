23db191df476587faf727665c2f9f53ee82848e3
ririndeismillia <ririndeismillia@gmail.com>
2026-03-02 22:22:10 +0700

    commit1

---
+++ fungsi.cpp
@@ -0,0 +1,12 @@
+//library
+#include <iostream>
+using namespace std;
+//deklarasi variabel
+int p, l;
+//implementasi dan prosedur
+void input(){
+    cout<< "Masukkan Panjang : ";
+    cin >> p;
+    cout << "Masukkan Lebar : ";
+    cin >> l; 
+}
